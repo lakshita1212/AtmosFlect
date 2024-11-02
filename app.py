d5494649324ef84f0cf8de1a306f5745a1276e04
Mlakshita12 <lakshitamadhavan12@gmail.com>
2024-11-02 18:38:06 -0400

    AQI finder for cities

--- app.py
+++ app.py
@@ -3,33 +3,36 @@ from dotenv import load_dotenv
 import os
 from flask import Flask, render_template, request, jsonify
 
-
-
 load_dotenv()
-api_key=os.getenv('API_KEY')
+api_key = os.getenv('API_KEY')
+
 app = Flask(__name__)
 
-
-
-def get_lat_long(city_name,state_code,country_code,API_key):
-    res=requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
+def get_lat_long(city_name, state_code, country_code, API_key):
+    res = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
     if res:
         latitude = res[0].get('lat')
         longitude = res[0].get('lon')
         print(f"Latitude: {latitude}, Longitude: {longitude}")
-        return latitude,longitude
+        return latitude, longitude 
     else:
         print("Location not found.")
-        return None,None
+        return None, None 
 
-def get_current_air_pollution(lat,lon,API_key):
-    res=requests.get(f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={API_key}').json()
-    print(res)
-
+def get_current_air_pollution(lat, lon, API_key):
+    res = requests.get(f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={API_key}').json()
+    return res  
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    print("Current Working Directory:", os.getcwd()) 
+    print("Templates Directory:", os.listdir('templates'))  
+    return render_template('index.html')  
+
+@app.route('/api_key')
+def get_api_key():
+    """Endpoint to get the API key."""
+    return jsonify({'api_key': api_key})
 
 @app.route('/get_data', methods=['GET'])
 def get_data():
@@ -39,15 +42,10 @@ def get_data():
     
     lat, lon = get_lat_long(city, state, country, api_key)
     if lat is not None and lon is not None:
-        weather_data = get_current_air_pollution(lat, lon, api_key)
-        return jsonify(weather_data) 
+        pollution_data = get_current_air_pollution(lat, lon, api_key)
+        return jsonify(pollution_data) 
     else:
         return jsonify({'error': 'Location not found'}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
- 
