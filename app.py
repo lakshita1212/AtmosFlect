56f0a57f05540142b63a25c6a0db4397213163e0
Mlakshita12 <lakshitamadhavan12@gmail.com>
2024-11-02 15:31:32 -0400

    test lat long function

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to Smog Tracker!"})
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
